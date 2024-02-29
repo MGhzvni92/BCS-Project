1cd024d199d0e73cc8c85d803c4091c640d2433d
miladhzz <miladhzz@gmail.com>
2024-03-01 01:51:05 +0330

    change cow picture folder

--- bcs/models.py
+++ bcs/models.py
@@ -2,13 +2,14 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-from django.db import models
+def upload_to(instance, filename):
+    return 'uploads/images/cow/{}_{}'.format(instance.code, filename)
 
 
 class Cow(models.Model):
     code = models.IntegerField(unique=True)
     title = models.CharField(max_length=100)
-    picture = models.ImageField(upload_to='cows/', blank=True)
+    picture = models.ImageField(upload_to=upload_to, blank=True)
     create_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 v1_urlpatterns = [    
@@ -13,5 +15,7 @@ urlpatterns = [
     path('bcs/', include('bcs.urls')),
     path('admin/', admin.site.urls),
     path('api/v1/', include(v1_urlpatterns)),
-    #url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT})
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
